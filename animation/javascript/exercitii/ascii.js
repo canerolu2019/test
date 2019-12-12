264ae04858cf5984f8c00475d2512f64a9799e6e
JavaScript <javascript@Admins-iMac-28.local>
2019-12-12 20:52:41 +0200

    jv 2

---
+++ animation/javascript/exercitii/ascii.js
@@ -0,0 +1,6 @@
+
+for(code = 0; code < 255; code++) {
+    document.write(code + " " + String.fromCharCode(code)); 
+    document.write("<br>"); 
+    code++;
+}
---
+++ animation/javascript/exercitii/inmultire.js
@@ -0,0 +1,8 @@
+
+for(var j = 1; j < 10; j++) {
+    for(var i = 1; i < 10; i++) {
+        document.write(1 + "x" + i + " = " + 1 * i) 
+        document.write("<br>")
+    }
+    document.write("<br>")
+}
