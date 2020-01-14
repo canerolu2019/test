1e05144938ea8629f8a81ab4bb62aa9717c52055
JavaScript <javascript@Admins-iMac-28.local>
2020-01-14 20:51:12 +0200

    js 6

--- animation/hangman/hangman.js
+++ animation/hangman/hangman.js
@@ -6,7 +6,7 @@ var game = {
         this.word = this.randomWorld(); 
     }, 
     randomWorld: function() {
-        let index = (Math.random() * this.words.length); 
+        let index = Math.floor(Math.random() * this.words.length); 
         return this.words[index];
     },
     tryLetter: function(letter) {
@@ -19,6 +19,33 @@ var game = {
         return this.triesLeft == 0;
     }, 
     isWinner: function() {
-        
-    }   
-}
+        return this.randomWorld.split('')
+                       .every(letter => this.letters.includes(letter))
+    }
+} 
+function renderLetters() {
+    document.getElementById("letters").innerText = game.letters.join(' '); 
+} 
+function renderWord() {
+    let text = game.word.split('') 
+        .map(letter => game.letters.includes(letter) ? letter : '_') 
+        .join(' ');
+        document.getElementById("word").innerText = text;
+} 
+function renderDrawing() {
+    let parts = ['head','body','left_arm','right_arm','left_leg','right_leg',] 
+    let visible = parts.slice(0, 6 - game.triesLeft); 
+    let invisible=parts.slice(6 - game.triesLeft); 
+    visible.forEach(name => document.getElementById(name).style.visibility = 'visible')
+} 
+function renderView() {
+    renderLetters();
+    renderWord(); 
+    renderDrawing(); 
+} 
+window.addEventListener('keydown', function(event) {
+    game.tryLetter(event.key); 
+    renderView(); 
+}); 
+game.start(); 
+renderView();
